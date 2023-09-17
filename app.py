d61586cf6fbd11cede8ea801715f8641fea99b1b
Brios Olivares <bolivares8090@gmail.com>
2023-09-16 22:28:22 -0400

    set up flask app structure & started implementing user signup/login with database

---
+++ app.py
@@ -0,0 +1,89 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, url_for
+from flask_login import login_user, login_required, LoginManager, current_user, UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__, template_folder='./templates')
+app.config['SECRET_KEY'] = 'my_secret_key'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key = True)
+  username = db.Column(db.String(80), unique = True, nullable = False)
+  email = db.Column(db.String(120), unique = True, nullable = False)
+  password = db.Column(db.String(120), nullable = False)
+
+  def set_password(self, password):
+    self.password = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.password, password)
+
+class SignUpForm(FlaskForm):
+  username = StringField('Username', validators=[DataRequired()])
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo(password)])
+  submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+  
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+  return render_template("dashboard.html")
+
+
+@app.route("/signup", methods = ['GET','POST'])
+def signup():
+  form = SignUpForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data, email=form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    login_user(user)
+    return redirect(url_for('login'))
+  return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        flash('Invalid email or password', 'danger')
+    return render_template('login.html', form=form)
+
+
+
+if __name__ == "__main__":
+  app.run() 
